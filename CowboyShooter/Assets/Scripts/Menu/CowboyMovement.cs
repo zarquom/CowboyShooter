c7669d723e86bb44825cb817ae02402d44d67e1c
Marcos Paulis <moonspell.doom@gmail.com>
2026-09-11 13:48:55 +0200

    Add animation for main menu

---
+++ CowboyShooter/Assets/Scripts/Menu/CowboyMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CowboyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private RectTransform rectTransform;
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if(rectTransform.anchoredPosition.x > 500f)
+        {
+            rectTransform.anchoredPosition = new Vector2(-500f, rectTransform.anchoredPosition.y);
+        }
+    }
+}
